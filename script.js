fd9c8270d27906a0a26063c2026077364b058d54
John <johntbrooks92@gmail.com>
2021-04-06 10:57:11 -0500

    12 characters total, working on new layout scheme

--- script.js
+++ script.js
@@ -2,46 +2,78 @@
 
 function goku() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/goku-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/vegeta-base.png)";
     var audio = document.getElementById("goku-audio");
     audio.play();
 }
 
 function vegeta() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/vegeta-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/frieza-base.png)";
     var audio = document.getElementById("vegeta-audio");
     audio.play();
 }
 
 function naruto() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/naruto-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/sasuke-base.png)";
     var audio = document.getElementById("naruto-audio");
     audio.play();
 }
 
 function sasuke() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/sasuke-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/kakashi-base.png)";
     var audio = document.getElementById("sasuke-audio");
     audio.play();
 }
 
 function superman() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/superman-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/batman-base.png)";
     var audio = document.getElementById("superman-audio");
     audio.play();
 }
 
 function batman() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/batman-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/superman-base.png)";
     var audio = document.getElementById("batman-audio");
     audio.play();
 }
 function obiwan() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/obi-wan-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/anakin-base.png)";
     var audio = document.getElementById("obi-wan-audio");
     audio.play();
 }
 function anakin() {
     document.getElementById("fighter-roster").style.backgroundImage = "url(img/anakin-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/obi-wan-base.png)";
     var audio = document.getElementById("anakin-audio");
     audio.play();
+}
+function optimus() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/optimus-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/megatron-base.png)";
+    // var audio = document.getElementById("obi-wan-audio");
+    // audio.play();
+}
+function megatron() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/megatron-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/optimus-base.png)";
+    // var audio = document.getElementById("anakin-audio");
+    // audio.play();
+}
+function frieza() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/frieza-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/goku-base.png)";
+    // var audio = document.getElementById("anakin-audio");
+    // audio.play();
+}
+function kakashi() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/kakashi-base.png)";
+    document.getElementById("fighter-roster-2").style.backgroundImage = "url(img/naruto-base.png)";
+    // var audio = document.getElementById("anakin-audio");
+    // audio.play();
 }
