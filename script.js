dde244c1fa731226dcb7b6328f25bb90bea54db4
John <johntbrooks92@gmail.com>
2021-04-06 08:52:31 -0500

    up to 8 characters rendered, made styling changes

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 "use strict";
 
 function goku() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/goku-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/goku-base.png)";
     var audio = document.getElementById("goku-audio");
     audio.play();
 }
 
 function vegeta() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/vegeta-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/vegeta-base.png)";
     var audio = document.getElementById("vegeta-audio");
     audio.play();
 }
 
 function naruto() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/naruto-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/naruto-base.png)";
     var audio = document.getElementById("naruto-audio");
     audio.play();
 }
 
 function sasuke() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/sasuke-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/sasuke-base.png)";
     var audio = document.getElementById("sasuke-audio");
     audio.play();
 }
 
 function superman() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/superman-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/superman-base.png)";
     var audio = document.getElementById("superman-audio");
     audio.play();
 }
 
 function batman() {
-    document.getElementById("character-box").style.backgroundImage = "url(img/batman-base.png)";
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/batman-base.png)";
     var audio = document.getElementById("batman-audio");
+    audio.play();
+}
+function obiwan() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/obi-wan-base.png)";
+    var audio = document.getElementById("obi-wan-audio");
+    audio.play();
+}
+function anakin() {
+    document.getElementById("fighter-roster").style.backgroundImage = "url(img/anakin-base.png)";
+    var audio = document.getElementById("anakin-audio");
     audio.play();
 }
