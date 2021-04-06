a4530506155d882eb6e0fa13dee7c570d4b35b7d
John <johntbrooks92@gmail.com>
2021-04-05 21:53:46 -0500

    2 character select screen rendering

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
 
+function goku() {
+    document.getElementById("character-box").style.backgroundImage = "url(img/goku-base.png)";
+    var audio = document.getElementById("goku-audio");
+    audio.play();
+}
+
+function vegeta() {
+    document.getElementById("character-box").style.backgroundImage = "url(img/vegeta-base.png)";
+    var audio = document.getElementById("vegeta-audio");
+    audio.play();
+}
